30c419ec37a1bdbd219d35bf054b67194a7784de
MateoCaicedoW <mcaicedo@wawand.co>
2024-02-18 16:09:44 -0500

    task: handle empty messages

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -8,6 +8,7 @@ function Chat() {
   const navigate = useNavigate()
   const [messages, setMessages] = useState([])
   const [message, setMessage] = useState('')
+  const [empty, setEmpty] = useState(false)
   const WS_URL = `wss://${import.meta.env.VITE_API_URL}/ws?Authorization=${import.meta.env.VITE_API_KEY}`
   const { sendJsonMessage, lastJsonMessage } = useWebSocket(
     WS_URL,
@@ -17,8 +18,19 @@ function Chat() {
     },
   )
 
+  const hanldeOnChange = (e) => {
+    setEmpty(false)
+    setMessage(e.target.value)
+  }
+
   const addMessage = (e) => {
     e.preventDefault()
+
+    if (message.trim() === '') {
+      setEmpty(true)
+      return
+    }
+
     sendJsonMessage({
       message: message,
       username: localStorage.getItem('email'),
@@ -26,6 +38,7 @@ function Chat() {
     })
 
     setMessage('')
+    setEmpty(false)
   }
 
   // Run when a new WebSocket message is received (lastJsonMessage)
@@ -75,7 +88,7 @@ function Chat() {
             </div>
         </div>
 
-        <div id="messages" className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch ">
+        <div id="messages" className="bg-gray-100 flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch ">
             {messages.map((message, index) => (
               <div key={index}>
                 {<Message message={message} />}
@@ -92,7 +105,7 @@ function Chat() {
                     </svg>
                   </button>
               </span> */}
-              <input type="text" value={message} placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3" onChange={e => setMessage(e.target.value)}/>
+              <input type="text" value={message} placeholder="Write your message!" className={"w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-3 bg-gray-200 rounded-md py-3 " + (empty ? ' border border-red-500' : '')} onChange={hanldeOnChange}/>
               <div className="absolute right-0 items-center inset-y-0 sm:flex">
                   {/* <button type="button" className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
