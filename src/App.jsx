fb59d39d172006b4a9be19d9fc3c5a7a4460cdd8
MateoCaicedoW <mcaicedo@wawand.co>
2024-02-18 15:41:21 -0500

    Update .gitignore and index.html, package.json, App.jsx, and Message.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
                     <Route path="/" element={<Register />} />
                     <Route path="/chat" element={<Chat />} />
                     <Route path="*" element={
-                    <section class="bg-white dark:bg-gray-900 ">
-                        <div class="container flex items-center min-h-screen px-6 py-12 mx-auto">
+                    <section className="bg-white dark:bg-gray-900 ">
+                        <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
                             <div>
-                                <p class="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
-                                <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">We can’t find that page</h1>
-                                <p class="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist or has been moved.</p>
+                                <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
+                                <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">We can’t find that page</h1>
+                                <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist or has been moved.</p>
 
-                                <div class="flex items-center mt-6 gap-x-3">
+                                <div className="flex items-center mt-6 gap-x-3">
                                 </div>
                             </div>
                         </div>
