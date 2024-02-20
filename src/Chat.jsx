bbcb06bc799829f22ec376ba1349aecbc8fb177e
MateoCaicedoW <mcaicedo@wawand.co>
2024-02-19 23:02:42 -0500

    Update WebSocket URL in Chat component

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -11,7 +11,7 @@ function Chat() {
   const [message, setMessage] = useState('')
   const [empty, setEmpty] = useState(false)
   const [showButton, setShowButton] = useState(false)
-  const WS_URL = `${import.meta.env.VITE_API_URL}/ws?Authorization=${import.meta.env.VITE_API_KEY}`
+  const WS_URL = `${import.meta.env.VITE_API_WEBSOCKET_URL}/ws?Authorization=${import.meta.env.VITE_API_KEY}`
   const { sendJsonMessage, lastJsonMessage } = useWebSocket(
     WS_URL,
     {
@@ -173,7 +173,7 @@ function Chat() {
               <div className="absolute right-0 items-center inset-y-0 sm:flex">
                   <label htmlFor='file' type="button" className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
                     </svg>
                     <input type="file" id='file' className='hidden' onChange={handleInputFile}/>
                   </label>
